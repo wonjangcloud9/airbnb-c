116251f694a145b5737287b7c6a7154bacdc154f
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-09-26 20:53:20 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+class Player:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}"
+
+player = Player("John", 20)
+print(player)
