49f63279648a7431617e908a068dec6bf2735db0
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-09-30 21:48:22 +0900

    modle

---
+++ houses/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class House(models.Model):
+
+    """ House Model Definition """
+
+    name = models.CharField(max_length=140)
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=140)
+
+
+
