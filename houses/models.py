06aa651ade161e2956dec660ebe251bd62dcaffd
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-10-01 17:13:50 +0900

    model & migration

--- houses/models.py
+++ houses/models.py
@@ -5,9 +5,10 @@ class House(models.Model):
     """ House Model Definition """
 
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=140)
+    pet_allowed = models.BooleanField(default=True)
 
 
 
