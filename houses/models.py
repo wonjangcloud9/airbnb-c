166db9aa46fbae93ce3f896df7122b905c85f647
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-10-02 10:36:50 +0900

    Doc...

--- houses/models.py
+++ houses/models.py
@@ -5,10 +5,14 @@ class House(models.Model):
     """ House Model Definition """
 
     name = models.CharField(max_length=140)
-    price_per_night = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField(verbose_name="Price", help_text="Positive Numbers Only")
     description = models.TextField()
     address = models.CharField(max_length=140)
-    pet_allowed = models.BooleanField(default=True)
+    pet_allowed = models.BooleanField(
+        verbose_name = "Pet Allowed",
+        default=True, 
+        help_text="Can you bring your pet?"
+        )
 
     def __str__(self):
         return self.name
