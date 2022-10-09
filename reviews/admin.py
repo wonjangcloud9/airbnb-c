948b449c3afece460787b3c9f9f453c530fd5317
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-10-09 21:06:20 +0900

    filter

--- reviews/admin.py
+++ reviews/admin.py
@@ -4,6 +4,27 @@ from .models import Review
 # Register your models here.
 
 
+class WordFilter(admin.SimpleListFilter):
+    title = "word"
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("15", "15"),
+            ("32", "32"),
+            ("123", "123"),
+        )
+
+    def queryset(self, request, queryset):
+        word = self.value()
+        if word == "15":
+            return queryset.filter(payload__contains="15")
+        elif word == "32":
+            return queryset.filter(payload__contains="32")
+        elif word == "123":
+            return queryset.filter(payload__contains="123")
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
@@ -11,4 +32,10 @@ class ReviewAdmin(admin.ModelAdmin):
         "payload",
     )
 
-    list_filter = ("rating",)
+    list_filter = (
+        WordFilter,
+        "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_allowed",
+    )
