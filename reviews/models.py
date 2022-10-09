a944af4916e9a160b3d41b07389f792428c7f3fc
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-10-09 18:40:55 +0900

    related

--- reviews/models.py
+++ reviews/models.py
@@ -11,18 +11,21 @@ class Review(CommonModel):
     user = models.ForeignKey(
         "users.User",
         on_delete=models.CASCADE,
+        related_name="reviews",
     )
     room = models.ForeignKey(
         "rooms.Room",
         on_delete=models.CASCADE,
         null=True,
         blank=True,
+        related_name="reviews",
     )
     experience = models.ForeignKey(
         "experiences.Experience",
         on_delete=models.CASCADE,
         null=True,
         blank=True,
+        related_name="reviews",
     )
 
     payload = models.TextField()
