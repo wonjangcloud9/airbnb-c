e1f93f748a9729ffd075583d056b1abab35767cd
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-10-10 19:28:28 +0900

    url args

--- rooms/views.py
+++ rooms/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def say_hello(request):
-    return HttpResponse("Hello World")
+def see_all_rooms(request):
+    return HttpResponse("All Rooms")
+
+
+def see_one_room(request, room_id):
+    return HttpResponse(f"Room {room_id}")
