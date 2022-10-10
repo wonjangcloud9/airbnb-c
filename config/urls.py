6c6471c6de4045662e5be63c9586a791c5519575
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-10-10 19:25:24 +0900

    include

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rooms import views as room_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("rooms", room_views.say_hello),
+    path("rooms/", include("rooms.urls")),
 ]
