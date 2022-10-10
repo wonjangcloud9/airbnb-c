c32c3df6e2572bc72847d9645657eb929376c4c3
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-10-10 19:53:55 +0900

    does not exist

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from cmath import exp
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Room
@@ -11,4 +12,10 @@ def see_all_rooms(request):
 
 
 def see_one_room(request, room_id):
-    return HttpResponse(f"Room {room_id}")
+    try:
+        room = Room.objects.get(pk=room_id)
+        return render(request, "room_detail.html", {"room": room, "title": room.name})
+    except Room.DoesNotExist:
+        return render(
+            request, "room_detail.html", {"not_found": True, "title": "Room Not Found"}
+        )
