fac10a12de21fcc8c785af594b3419cbfddba03e
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-10-15 16:00:27 +0900

    amenity detail

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("amenities/", views.Amenities.as_view(), name="amenity-list"),
-    path("amenities/<int:pk>", views.AmenityDetail.as_view(), name="amenity-detail"),
+    path("amenities/", views.Amenities.as_view()),
+    path("amenities/<int:pk>", views.AmenityDetail.as_view()),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from .models import Amenity
 from .serializers import AmenitySerializer
 
@@ -16,15 +17,34 @@ class Amenities(APIView):
             serializer.save()
             return Response(data=serializer.data)
         else:
-            return Response(status=400)
+            return NotFound
 
 
 class AmenityDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Amenity.objects.get(pk=pk)
+        except Amenity.DoesNotExist:
+            return Response(status=404)
+
     def get(self, request, pk):
-        pass
+        try:
+            amenity = Amenity.objects.get(pk=pk)
+            serializer = AmenitySerializer(amenity)
+            return Response(data=serializer.data)
+        except Amenity.DoesNotExist:
+            return Response(status=404)
 
     def put(self, request, pk):
-        pass
+        amenity = self.get_object(pk)
+        serializer = AmenitySerializer(amenity, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data)
+        else:
+            return Response(status=404)
 
     def delete(self, request, pk):
-        pass
+        amenity = self.get_object(pk)
+        amenity.delete()
+        return Response(status=204)
