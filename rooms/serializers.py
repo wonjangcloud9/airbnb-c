37e9fa75418a52ed4a2a571cd2feb14856bd0ea4
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-10-16 16:21:34 +0900

    Reverse Serializers

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from reviews.serializers import ReviewSerializer
 from .models import Amenity, Room
 from users.serializers import TinyUserSerializer
 from categories.serializers import CategorySerializer
@@ -45,6 +47,7 @@ class RoomDetailSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     rating = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
+    reviews = ReviewSerializer(many=True, read_only=True)
 
     class Meta:
         model = Room
