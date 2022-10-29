5c8c06b2007c000ed3be962a6a434f65174e04d4
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-10-29 18:38:19 +0900

    wishlist

---
+++ wishlists/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+from rooms.serializers import RoomListSerializer
+from .models import WishList
+
+
+class WishListSerializer(ModelSerializer):
+
+    rooms = RoomListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = WishList
+        fields = (
+            "name",
+            "rooms",
+        )
--- wishlists/views.py
+++ wishlists/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import WishList
+from .serializers import WishListSerializer
 
-# Create your views here.
+
+class Wishlists(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        all_wishlists = WishList.objects.filter(user=request.user)
+        print(request.user)
+        serializer = WishListSerializer(
+            all_wishlists,
+            many=True,
+            context={"request": request},
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = WishListSerializer(data=request.data)
+        if serializer.is_valid():
+            wishlist = serializer.save(
+                user=request.user,
+            )
+            serializer = WishListSerializer(wishlist)
+            return Response(serializer.data)
+        return Response(serializer.errors)
