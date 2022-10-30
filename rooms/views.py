2ef1e00b1d14773a48b290a28243872316e58dcb
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-10-30 16:44:56 +0900

    see booking list

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.utils import timezone
 from django.db import transaction
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -10,6 +11,8 @@ from categories.models import Category
 from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
+from bookings.models import Booking
+from bookings.serializers import PublicBookingSerializer
 
 
 class Amenities(APIView):
@@ -210,3 +213,29 @@ class RoomPhotos(APIView):
             return Response(data=serializer.data)
         else:
             return Response(status=400, data={"error": "Bad Request"})
+
+
+class RoomBookings(APIView):
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            return Response(status=404, data={"error": "Room not found"})
+
+    def get(self, request, pk):
+        room = self.get_object(pk)
+        now = timezone.localtime(timezone.now()).date()
+
+        bookings = Booking.objects.filter(
+            room=room,
+            kind=Booking.BookingKindChoices.ROOM,
+            check_in__gt=now,
+        )
+        serializer = PublicBookingSerializer(
+            bookings,
+            many=True,
+        )
+        return Response(data=serializer.data)
