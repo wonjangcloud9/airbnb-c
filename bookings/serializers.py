7ed9fd94d3ef5366fa151d26544afdb7f1ca5442
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-10-30 17:03:20 +0900

    create booking

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,5 +1,45 @@
+from django.utils import timezone
 from rest_framework import serializers
 from .models import Booking
+
+
+class CreateRoomBookingSerializer(serializers.ModelSerializer):
+
+    check_in = serializers.DateField()
+    check_out = serializers.DateField()
+
+    class Meta:
+        model = Booking
+        fields = (
+            "check_in",
+            "check_out",
+            "guests",
+        )
+
+    def validate_check_in(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise serializers.ValidationError("Check in date should be after today")
+        return value
+
+    def validate_check_out(self, value):
+        now = timezone.localtime(timezone.now()).date()
+        if now > value:
+            raise serializers.ValidationError("Check in date should be after today")
+        return value
+
+    def create(self, validated_data):
+        guests = self.context.get("guests")
+        room = validated_data.get("room")
+        check_in = validated_data.get("check_in")
+        check_out = validated_data.get("check_out")
+        booking = Booking.objects.create(
+            guests=guests,
+            room=room,
+            check_in=check_in,
+            check_out=check_out,
+        )
+        return booking
 
 
 class PublicBookingSerializer(serializers.ModelSerializer):
--- rooms/views.py
+++ rooms/views.py
@@ -12,7 +12,7 @@ from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerial
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
 from bookings.models import Booking
-from bookings.serializers import PublicBookingSerializer
+from bookings.serializers import PublicBookingSerializer, CreateRoomBookingSerializer
 
 
 class Amenities(APIView):
@@ -239,3 +239,17 @@ class RoomBookings(APIView):
             many=True,
         )
         return Response(data=serializer.data)
+
+    def post(self, request, pk):
+        room = self.get_object(pk)
+        serializer = CreateRoomBookingSerializer(data=request.data)
+        if serializer.is_valid():
+            booking = serializer.save(
+                room=room,
+                user=request.user,
+                kind=Booking.BookingKindChoices.ROOM,
+            )
+            serializer = PublicBookingSerializer(booking)
+            return Response(data=serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
