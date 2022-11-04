e9fb754f20f89ed5583fb8a066962683de8970c3
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-11-04 20:05:46 +0900

    log-in & log-out

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -68,3 +69,32 @@ class ChangePassword(APIView):
             return Response(status=status.HTTP_200_OK)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogIn(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError("Username and password are required")
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        if user:
+            login(request, user)
+            return Response({"ok": "welcome"}, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={"error": "Invalid username or password"},
+            )
+
+
+class LogOut(APIView):
+    def post(self, request):
+        permission_classes = [IsAuthenticated]
+
+        logout(request)
+        return Response(status=status.HTTP_200_OK, data={"ok": "bye"})
