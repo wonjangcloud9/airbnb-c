35f8164420326078185e9221c05db8ea8425102e
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-11-04 19:30:49 +0900

    create user

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ParseError
 from . import models, serializers
 
 
@@ -22,5 +23,21 @@ class Me(APIView):
             user = serializer.save()
             serializer = serializers.PrivateUserSerializer(user)
             return Response(data=serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError("Password is required")
+        serializer = serializers.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
