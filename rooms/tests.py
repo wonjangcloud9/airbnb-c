a7fadf20bf3aca93130b42c646f2cc14e77c07d7
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-11-05 16:35:22 +0900

    amenities test

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,7 +1,20 @@
 from rest_framework.test import APITestCase
+from . import models
 
 
 class TestAmenities(APITestCase):
+
+    NAME = "Amenity Test"
+    DESCRIPTION = "Amenity Test Description"
+
+    def setUp(self):
+        models.Amenity.objects.create(name=self.NAME, description=self.DESCRIPTION)
+
     def test_get_amenities(self):
         response = self.client.get("/api/v1/rooms/amenities")
-        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(response.status_code, 200, "Status code should be 200")
+        self.assertIsInstance(data, list, "Data should be a list")
+        self.assertEqual(len(data), 1, "Data should have 1 item")
+        self.assertEqual(data[0]["name"], self.NAME)
+        self.assertEqual(data[0]["description"], self.DESCRIPTION)
